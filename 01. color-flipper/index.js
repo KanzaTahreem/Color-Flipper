7180e033f8b6af17d6545e76c9979294adb4e843
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-06-11 17:21:08 +0500

    Update to get a random number

--- 01. color-flipper/index.js
+++ 01. color-flipper/index.js
@@ -1,10 +1,16 @@
-let simple = ['green', 'red', 'orange', 'blue', 'pruple']
+let simple = ['green', 'red', 'orange', 'blue', 'lightblue', 'indigo', 'pink', 'grey', 'yellow', 'goldenrod', 'violet', 'olive', 'navy', 'teal', 'aqua']
 let btn = document.getElementById('btn')
 let color = document.getElementById('color')
 
 btn.addEventListener('click', function(){
-    // get random number between 0 - 3
-    let randomNumber = 2;
+    let randomNumber = getRandomNumber();
     document.body.style.backgroundColor = simple[randomNumber]
     color.textContent = simple[randomNumber]
 })
+
+    // make it more interesting by getting a random number from simple (array)
+    // we can do this by setting up a function that wi generate a random number
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * simple.length )
+}
